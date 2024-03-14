00eaf469f77ef306e5c6e0e3706d90608fd8c225
giorgia-DNicola <giorgia.dnicola@gmail.com>
2024-03-14 13:51:38 +0100

    reader csv file

--- assignment1/src/main/java/com/atcs/Main.java
+++ assignment1/src/main/java/com/atcs/Main.java
@@ -1,8 +1,26 @@
 package com.atcs;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World");
+        try (CSVReader reader = new CSVReader(new FileReader("src/main/resources/data/ml-latest-small/ratings.csv"))) {
+            List<String[]> rows = reader.readAll();
+            System.out.println("Total number of ratings expected: 100836 , actual number of ratings: " + (rows.size()-1)); 
+            
+//            for (String[] row : rows) {
+//                for (String cell : row) {
+//                    System.out.print(cell + "\t");
+//                }
+//                System.out.println();
+//            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
     }
 }
