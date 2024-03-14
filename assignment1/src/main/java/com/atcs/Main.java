a2c02d29d89457f3d9beb43e2881bba0ec0ff8ae
kaguyasama <fede97d@hotmail.it>
2024-03-14 12:56:37 +0100

    set up completed

---
+++ assignment1/src/main/java/com/atcs/Main.java
@@ -0,0 +1,8 @@
+package src.main.java.com.atcs;
+
+public class Main {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+}
