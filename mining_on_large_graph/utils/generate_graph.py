2510b08072b19ed06c4a73dc8e22d0811ef47ded
Giorgia-DNicola <giorgia.dnicola@gmail.com>
2024-06-15 22:22:11 +0200

    final commit

--- mining_on_large_graph/utils/generate_graph.py
+++ mining_on_large_graph/utils/generate_graph.py
@@ -5,21 +5,27 @@ import matplotlib.pyplot as plt
 # Carica i dati dal file Excel dei risultati
 df_results = pd.read_excel('./data/partnerships_data.xlsx', engine='openpyxl')
 
+print("Nomi delle colonne: ", df_results.columns)
+
 G = nx.Graph()
 
 # Aggiungi nodi agli streamer
 for index, row in df_results.iterrows():
-    G.add_node(row['StreamerID'])
+    G.add_node(row['numeric_id_1'])
+    G.add_node(row['numeric_id_2'])
 
 # Aggiungi archi per le partnership con attributi
 for index, row in df_results.iterrows():
-    G.add_edge(row['StreamerID1'], row['StreamerID2'], similarity=row['Similarity'], partnership=row['Partnership'])
+    G.add_edge(row['numeric_id_1'], row['numeric_id_2'], similarity=row['Follower similarity score'], partnership=row['Partnership'])
 
-nx.draw(G, with_labels=True)
+plt.figure(flgsize=(10,8))
+nx.draw(G, with_labels=True, node_size=50, font_size=8)
 plt.show()
 
 communities = nx.algorithms.community.greedy_modularity_communities(G)
+print("Comunita trovate:", communities)
 
 degree_centrality = nx.algorithms.centrality.degree_centrality(G)
+print("Centralità di grado:", degree_centrality)
 
 nx.write_gpickle(G, './data/streamer_graph.gpickle')
--- mining_on_large_graph/utils/twitch_streamer_analysis.py
+++ mining_on_large_graph/utils/twitch_streamer_analysis.py
@@ -1,82 +1,107 @@
 import pandas as pd
-import time
+import csv
 from concurrent.futures import ProcessPoolExecutor
+import xlsxwriter
+from datetime import datetime
+from models.node import Node  # Importa la classe Node dal modulo node.py
 
 class StreamerPartnershipAnalyzer:
     
-    def __init__(self, features_file='./data/large_twitch_edges.csv', followers_file='./data/large_twitch_followers.csv'):
-        print("Loading features and followers data...")
-        start_time = time.time()
+    def __init__(self, edges_file='./data/large_twitch_edges.csv', features_file='./data/large_twitch_features.csv'):
+        print("Loading edges and features data...")
+        self.df_edges = pd.read_csv(edges_file)
         self.df_features = pd.read_csv(features_file)
-        self.df_followers = pd.read_csv(followers_file)
-        self.followers_dict = self.df_followers.set_index('numeric_id').to_dict(orient='index')
-        end_time = time.time()
-        print(f"Data loaded successfully in {end_time - start_time:.2f} seconds.")
+        
+        # Convert numeric_id to string
+        self.df_edges['numeric_id_1'] = self.df_edges['numeric_id_1'].astype(str)
+        self.df_edges['numeric_id_2'] = self.df_edges['numeric_id_2'].astype(str)
+        self.df_features['numeric_id'] = self.df_features['numeric_id'].astype(str)
+        
+    @staticmethod
+    def update_followers(file_path, nodes_dict):
+        with open(file_path, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip the header
+            for row in reader:
+                if row:
+                    primary_id = row[0]
+                    if primary_id in nodes_dict:
+                        nodes_dict[primary_id].followers += 1
     
     def calculate_follower_similarity(self, follower1, follower2):
         return 1 / (abs(follower1 - follower2) + 1)
     
     def process_batch(self, batch):
-        print(f"Processing batch with {len(batch)} rows...")
         results = []
-        for index, row in batch.iterrows():
+        
+        nodes_dict = {}
+        for _, row in self.df_features.iterrows():
+            node = Node(
+                views=row['views'],
+                mature=row['mature'],
+                life_time=row['life_time'],
+                created_at=row['created_at'],
+                updated_at=row['updated_at'],
+                numeric_id=row['numeric_id'],
+                dead_account=row['dead_account'],
+                language=row['language'],
+                affiliate=row['affiliate']
+            )
+            nodes_dict[row['numeric_id']] = node
+        
+        for _, row in batch.iterrows():
             numeric_id_1 = row['numeric_id_1']
             numeric_id_2 = row['numeric_id_2']
-
-            info_1 = self.followers_dict.get(numeric_id_1)
-            info_2 = self.followers_dict.get(numeric_id_2)
-
-            if not info_1 or not info_2:
-                results.append({
-                    'Language similarity': '-',
-                    'Follower similarity': '-',
-                    'Language similarity score': '-',
-                    'Follower similarity score': '-',
-                    'Partnership': 0
-                })
-                continue
             
-            language1 = info_1['language']
-            language2 = info_2['language']
-            followers1 = info_1['followers']
-            followers2 = info_2['followers']
+            info_1 = nodes_dict.get(numeric_id_1)
+            info_2 = nodes_dict.get(numeric_id_2)
             
-            if language1==language2: 
-                language_similarity=1
-            else :
-                language_similarity=0
+            #if not info_1:
+                #print(f"Missing data for numeric_id: {numeric_id_1}")
+                #continue
+            
+            #if not info_2:
+                #print(f"Missing data for numeric_id: {numeric_id_2}")
+                #continue
+            
+            language1 = info_1.language
+            language2 = info_2.language
+            followers1 = info_1.followers
+            followers2 = info_2.followers
+            
+            language_similarity = 1 if language1 == language2 else 0
             follower_similarity = self.calculate_follower_similarity(followers1, followers2)
             
             results.append({
+                'numeric_id_1': numeric_id_1,
+                'numeric_id_2': numeric_id_2,
                 'Language similarity': f"{language1}, {language2}",
                 'Follower similarity': f"{followers1}, {followers2}",
                 'Language similarity score': language_similarity,
                 'Follower similarity score': follower_similarity,
-                'Partnership': 1 if language_similarity == 1.0 and follower_similarity > 0.1 else 0
+                'Partnership': 1 if language_similarity == 1 and follower_similarity > 0.0005 else 0
             })
-        print(f"Finished processing batch with {len(batch)} rows.")
+        
         return results
+
     
     def find_potential_partnerships(self, output_excel='./data/partnerships_data.xlsx', batch_size=10000):
-        num_batches = len(self.df_features) // batch_size + 1
-        print(f"Starting to process {len(self.df_features)} rows in {num_batches} batches...")
+        self.update_followers('./data/large_twitch_edges.csv', self.df_features)
 
+        num_batches = len(self.df_edges) // batch_size + 1
         all_results = []
 
         with ProcessPoolExecutor() as executor:
             futures = []
-            for batch_start in range(0, len(self.df_features), batch_size):
-                batch_end = min(batch_start + batch_size, len(self.df_features))
-                batch = self.df_features.iloc[batch_start:batch_end]
-                print(f"Submitting batch {batch_start // batch_size + 1} to executor...")
+            for batch_start in range(0, len(self.df_edges), batch_size):
+                batch_end = min(batch_start + batch_size, len(self.df_edges))
+                batch = self.df_edges.iloc[batch_start:batch_end]
                 future = executor.submit(self.process_batch, batch)
                 futures.append(future)
 
             for future in futures:
                 all_results.extend(future.result())
-                print("Batch processed and results collected.")
                 
-        # Save results to multiple sheets if necessary
         max_rows_per_sheet = 1048576
         num_sheets = len(all_results) // max_rows_per_sheet + 1
 
@@ -86,6 +111,5 @@ class StreamerPartnershipAnalyzer:
                 end_row = min((i + 1) * max_rows_per_sheet, len(all_results))
                 df_results = pd.DataFrame(all_results[start_row:end_row])
                 df_results.to_excel(writer, sheet_name=f'Sheet{i+1}', index=False)
-                print(f"Sheet {i+1} saved with rows from {start_row} to {end_row}")
 
         print(f"Results saved to {output_excel}")
