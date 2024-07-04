5811b171d485f89bf987c8a2a1e3c32651dc3304
giorgia-DNicola <giorgia.dnicola@gmail.com>
2024-07-04 23:49:45 +0200

    fix data type

--- big_data_integration/main.py
+++ big_data_integration/main.py
@@ -1,23 +1,34 @@
-from linktransformer import LinkTransformer
+import linktransformer as lt
+import pandas as pd
 import json
 
-# Carica i dati JSON
-with open('0.json') as f:
-    data1 = json.load(f)
-
-with open('1.json') as f:
-    data2 = json.load(f)
+# Carica i dati CSV come DataFrame
+df1 = pd.read_csv('./schema/df1.csv')
+df2 = pd.read_csv('./schema/df2.csv')
 
 # Carica i campi da allineare dal file JSON
-with open('fields_to_align.json') as f:
+with open('./schema/tem_schema.json') as f:
     fields_to_align = json.load(f)
 
-# Inizializza LinkTransformer con le configurazioni necessarie
-link_transformer = LinkTransformer(fields=fields_to_align)
+# Funzione per unire i DataFrame sui campi specificati
+def merge_on_fields(df1, df2, fields, model="all-MiniLM-L6-v2"):
+    merged_df_list = []
+    for field_group, columns in fields.items():
+        for column in columns:
+            if column in df1.columns and column in df2.columns:
+                merged_df = lt.merge(df1, df2,
+                                     merge_type='1:1',
+                                     left_on=column,
+                                     right_on=column,
+                                     model=model)
+                merged_df_list.append(merged_df)
+    # Combina tutti i DataFrame uniti
+    final_merged_df = pd.concat(merged_df_list, axis=1)
+    final_merged_df = final_merged_df.loc[:,~final_merged_df.columns.duplicated()]
+    return final_merged_df
 
-# Esegui il collegamento dei record
-aligned_data = link_transformer.align([data1, data2])
+# Esegui il merge dei DataFrame utilizzando i campi specificati
+merged_df = merge_on_fields(df1, df2, fields_to_align)
 
-# Salva i dati allineati in un nuovo file JSON
-with open('aligned_data.json', 'w') as f:
-    json.dump(aligned_data, f, indent=4)
+# Salva i dati uniti in un nuovo file CSV
+merged_df.to_csv('./schema/aligned_data.csv', index=False)
